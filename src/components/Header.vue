4e730f3fd0aad0b9d232fab0f4e310243e8270fa
Zein Ersyad <zeinersyad@gmail.com>
2020-01-11 23:56:53 +0700

    update header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,40 +2,53 @@
   <v-container color="primary">
     <v-parallax src="../assets/header.jpg">
       <v-overlay :opacity="0.1" absolute>
-        <v-card
-          light
-          class="display-2"
-          align="center"
-          style="padding:16px;"
-          width="350"
-          elevation="12"
-          :ripple="false"
-          @click="show = !show"
-        >
-          <p class="display-3">Hello</p>
-          <transition name="slide-fade" mode="out-in">
-            <p v-if="show" class="subtitle-1" key="university">
-              I'm a student at
-              <strong>Telkom University</strong>
-            </p>
+        <v-hover v-slot:default="{ hover }">
+          <v-card
+            light
+            class="display-2"
+            align="center"
+            style="padding:16px;"
+            width="350"
+            hover="true"
+            :elevation="hover ? 12 : 2"
+            :ripple="false"
+            @click="show = !show"
+          >
+            <p class="display-3">Hello</p>
+            <transition name="slide-fade" mode="out-in">
+              <p v-if="show" class="subtitle-1" key="university">
+                I'm a student at
+                <strong>Telkom University</strong>
+              </p>
 
-            <p v-if="!show" class="subtitle-1" key="name">
-              My name is
-              <strong>Muhammad Zein Ersyad</strong>
-            </p>
-          </transition>
-        </v-card>
+              <p v-if="!show" class="subtitle-1" key="name">
+                My name is
+                <strong>Muhammad Zein Ersyad</strong>
+              </p>
+            </transition>
+          </v-card>
+        </v-hover>
         <transition name="slide-fade" mode="out-in">
-          <v-progress-linear v-if="show" value="80" height="25" background-opacity="0.5" rounded>
+          <v-progress-linear
+            v-if="show"
+            value="90"
+            height="25"
+            background-opacity="0.5"
+            rounded
+          >
             <strong class="body-1" style="color:white;">
-              <strong>Study Progress</strong> 80%
+              <strong>Study Progress</strong> 90%
             </strong>
           </v-progress-linear>
 
           <div v-if="!show" style="height:25px">
             <v-row justify="center">
-              <v-col v-for="interest in interests" :key="interest" align="center">
-                <v-chip color="primary" :ripple="false">{{interest}}</v-chip>
+              <v-col
+                v-for="interest in interests"
+                :key="interest"
+                align="center"
+              >
+                <v-chip color="primary" :ripple="false">{{ interest }}</v-chip>
               </v-col>
             </v-row>
           </div>
@@ -50,7 +63,7 @@ export default {
   name: "Header",
   data: () => ({
     show: false,
-    interests: ['Programming', 'Badminton', 'Gaming']
+    interests: ["Programming", "Badminton", "Gaming"]
   })
 };
 </script>
